adf8e78b3b50ea94b1ec81265f2a29b6e90558b7
Kamille Norris <kamionorato@gmail.com>
2020-06-12 11:43:40 -0400

    Add ant design, cards for repositories

--- src/Repositories/Repositories.tsx
+++ src/Repositories/Repositories.tsx
@@ -1,16 +1,19 @@
-import React, { ReactElement } from 'react';
+import { Card, Col, Row } from 'antd';
+import React from 'react';
 import { useQuery } from 'react-apollo-hooks';
 import gql from 'graphql-tag';
-import { Repository } from '../generated/graphql';
+import { GetRepositoriesQuery } from '../generated/graphql';
+import classes from './Repositories.module.css';
 
 const REPOSITORIES_QUERY = gql`
   query getRepositories {
     viewer {
-      repositories (last: 100, isFork: false) {
+      repositories (last: 100, isFork: false, privacy: PUBLIC) {
         nodes {
           name
           description
           url
+          id
           languages(first: 5) {
             nodes {
               color
@@ -24,15 +27,39 @@ const REPOSITORIES_QUERY = gql`
 `;
 
 const Repositories:React.FC = ():React.ReactElement => {
-
-  const { loading, error, data } = useQuery<Repository>(REPOSITORIES_QUERY);
+  const { loading, error, data } = useQuery<GetRepositoriesQuery>(REPOSITORIES_QUERY);
+  let content;
 
   if (loading) return <div>Loading...</div>;
   if (error) return <div>Error! {JSON.stringify(error)}</div>;
 
+  if (data) {
+    const repositories = data.viewer.repositories.nodes;
+    content = repositories && repositories.map(repo => {
+      return (
+        <Col span={ 8 } key={repo?.name}>
+          <Card title={ repo?.name } bordered={ false }>
+            <p>{ repo?.description }</p>
+            <p>
+              <a
+                href={repo?.url}
+                rel="noopener noreferrer"
+                target="_blank"
+              >
+                {repo?.url}
+              </a>
+            </p>
+          </Card>
+        </Col>
+      )
+    });
+  }
+
   return (
-    <div>
-      { JSON.stringify(data) }
+    <div className={classes['site-card-wrapper']}>
+      <Row gutter={16}>
+        {content}
+      </Row>
     </div>
   );
 };
